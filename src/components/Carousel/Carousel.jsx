f874da1b4f14de406d1cb4c198cc2bd74b0dbc59
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2024-01-22 22:20:02 -0800

    Home = > Navbar

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+
+function Carousel() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Carousel
---
+++ src/constants/images.jsx
@@ -0,0 +1,8 @@
+// NavBar
+import NavbarLogo from "../assets/NavBar/Navbar-Logo.png"
+
+
+const images = {
+    NavbarLogo,
+}
+export default images
