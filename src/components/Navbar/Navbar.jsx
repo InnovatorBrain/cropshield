352962e426888a859d23dada07aed1b05697f0f1
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2024-02-03 08:27:33 -0800

    Navbar dropdown setting < frontend

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import images from '../../constants/images'
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 import { IoPersonSharp } from "react-icons/io5";
 import { TfiClose } from "react-icons/tfi";
@@ -41,7 +42,19 @@ const Navbar = () => {
           <ul className="app__navbar-links">
             <li className='p-links'><Link to="/Home">Home</Link></li>
             <li className='p-links'><Link to="/AboutUs">About Us</Link></li>
-            <li className='p-links'><Link to="/Gallery">Gallery</Link></li>
+            {/* <li className='p-links'><Link to="/Gallery">Insurance</Link></li> */}
+            <div>
+              <div className="dropdown">
+                <ul className='p-links' id='nav-drop-aligning'>
+                  <li><Link to="/Gallery">Insurance</Link><RiArrowDropDownLine className="dropdown-icon" /></li>
+                </ul>
+                <div className="dropdown-content">
+                  <a href="Insurance">Insurance</a>
+                  <a href="/ApplyOnline">Apply Online</a>
+                  <a href="/InsuranceClaims">Insurance claims</a>
+                </div>
+              </div>
+            </div>
             <li className='p-links'><Link to="/Menu">Menu</Link></li>
             <li className='p-links'><Link to="/Contact">Contact</Link></li>
             <li className='p-links'><Link to="/Shop">Shop</Link></li>
@@ -60,7 +73,19 @@ const Navbar = () => {
                 <ul className="app__navbar-smallscreen_links">
                   <li><Link to="/Home" onClick={() => setToggleMenu(false)}>Home</Link></li>
                   <li><Link to="/AboutUs" onClick={() => setToggleMenu(false)}>AboutUs</Link></li>
-                  <li><Link to="/Gallery" onClick={() => setToggleMenu(false)}>Gallery</Link></li>
+                  {/* <li><Link to="/Gallery" onClick={() => setToggleMenu(false)}>Gallery</Link></li> */}
+                  <li className='nav-drop-in-togle'>
+                    <div className="dropdown">
+                      <ul className='p-links' id='nav-drop-aligning'>
+                        <li><Link to="">Insurance</Link><RiArrowDropDownLine className="dropdown-icon" /></li>
+                      </ul>
+                      <div className="dropdown-content">
+                        <a href="/Insurance">Insurance</a>
+                        <a href="/ApplyOnline">Apply Online</a>
+                        <a href="/InsuranceClaims">Insurance claims</a>
+                      </div>
+                    </div>
+                  </li>
                   <li><Link to="/Menu" onClick={() => setToggleMenu(false)}>Menu</Link></li>
                   <li><Link to="/Contact" onClick={() => setToggleMenu(false)}>Contact</Link></li>
                   <li><Link to="/Shop" onClick={() => setToggleMenu(false)}>Shop</Link></li>
